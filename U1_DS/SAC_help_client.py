4461a97d85ea4bf99aaa26c28560c50cf7560f5d
Angel Pons <th3fanbus@gmail.com>
2025-10-08 16:41:30 +0200

    Revert "Use tabs instead of spaces"
    This reverts commit f4dcee678332abd9fcc0ef764b859f38c14f3862.
    
    Reason: editor thinks tabs are 8 spaces.

--- U1_DS/SAC_help_client.py
+++ U1_DS/SAC_help_client.py
@@ -24,88 +24,88 @@ RESP_TIMEOUT_SECONDS = 3
 
 # The lowest number of accepted help requests
 def min_accepted_help_req_num(num_helpers):
-	return num_helpers // 2 + 1
+    return num_helpers // 2 + 1
 
 # Check the case of 3 helpers (what the instructions say)
 assert min_accepted_help_req_num(3) == 2
 
 class Client:
-	def __init__(self, hosts_ports):
-		# Create a socket (SOCK_STREAM means a TCP socket)
-		self.sockets = {socket.socket(socket.AF_INET, socket.SOCK_STREAM): host_port for host_port in hosts_ports}
+    def __init__(self, hosts_ports):
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        self.sockets = {socket.socket(socket.AF_INET, socket.SOCK_STREAM): host_port for host_port in hosts_ports}
 
-	def __enter__(self):
-		for sock, host_port in self.sockets.items():
-			sock.__enter__()
-			print(f"Connecting to '{host_port[0]}:{host_port[1]}'...")
-			sock.connect(host_port)
-		print("Connected!")
-		return self
+    def __enter__(self):
+        for sock, host_port in self.sockets.items():
+            sock.__enter__()
+            print(f"Connecting to '{host_port[0]}:{host_port[1]}'...")
+            sock.connect(host_port)
+        print("Connected!")
+        return self
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		for sock, host_port in self.sockets.items():
-			sock.close()
-			sock.__exit__(exc_type, exc_val, exc_tb)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for sock, host_port in self.sockets.items():
+            sock.close()
+            sock.__exit__(exc_type, exc_val, exc_tb)
 
-	# Handle data from server
-	def handle_response(self, sock):
-		return sock.recv(1024).decode("utf-8").rstrip()
+    # Handle data from server
+    def handle_response(self, sock):
+        return sock.recv(1024).decode("utf-8").rstrip()
 
-	def get_responses(self, sel):
-		init_time = time.monotonic()
-		responses = {}
-		got_help = False
-		while True:
-			events = sel.select(RESP_TIMEOUT_SECONDS)
-			# We don't use event masks
-			for key, _ in events:
-				callback = key.data
-				sock = key.fileobj
-				responses.update({sock: callback(sock)})
-			# Check if we have enough ok responses
-			num_ok_responses = 0
-			for _, data in responses.items():
-				if data == "ok!":
-					num_ok_responses += 1
-			if num_ok_responses >= min_accepted_help_req_num(len(self.sockets)):
-				got_help = True
-				break
-			# All responses acquired, no need to wait any further
-			if len(responses) == len(self.sockets):
-				break
-			curr_time = time.monotonic()
-			if curr_time - init_time > RESP_TIMEOUT_SECONDS:
-				break
-		print(f"Got {len(responses)} responses:")
-		for sock, data in responses.items():
-			print(f"[{sock}]:\t{data}")
-		return got_help
+    def get_responses(self, sel):
+        init_time = time.monotonic()
+        responses = {}
+        got_help = False
+        while True:
+            events = sel.select(RESP_TIMEOUT_SECONDS)
+            # We don't use event masks
+            for key, _ in events:
+                callback = key.data
+                sock = key.fileobj
+                responses.update({sock: callback(sock)})
+            # Check if we have enough ok responses
+            num_ok_responses = 0
+            for _, data in responses.items():
+                if data == "ok!":
+                    num_ok_responses += 1
+            if num_ok_responses >= min_accepted_help_req_num(len(self.sockets)):
+                got_help = True
+                break
+            # All responses acquired, no need to wait any further
+            if len(responses) == len(self.sockets):
+                break
+            curr_time = time.monotonic()
+            if curr_time - init_time > RESP_TIMEOUT_SECONDS:
+                break
+        print(f"Got {len(responses)} responses:")
+        for sock, data in responses.items():
+            print(f"[{sock}]:\t{data}")
+        return got_help
 
-	def main_loop(self):
-		while True:
-			with selectors.DefaultSelector() as sel:
-				for sock, _ in self.sockets.items():
-					sock.sendall("help!".encode("utf-8"))
-					sel.register(sock, selectors.EVENT_READ, self.handle_response)
-				print()
-				print("Asking for help!")
-				got_help = self.get_responses(sel)
-				# Log result of getting responses
-				print()
-				if got_help:
-					print("Got enough help!!!")
-					return
-				print("Did not receive enough help...")
-				print()
-				print("Zzzzz...")
-				time.sleep(random.randint(1, 1)) # Range changed for convenience of testing
-				print()
-				print()
+    def main_loop(self):
+        while True:
+            with selectors.DefaultSelector() as sel:
+                for sock, _ in self.sockets.items():
+                    sock.sendall("help!".encode("utf-8"))
+                    sel.register(sock, selectors.EVENT_READ, self.handle_response)
+                print()
+                print("Asking for help!")
+                got_help = self.get_responses(sel)
+                # Log result of getting responses
+                print()
+                if got_help:
+                    print("Got enough help!!!")
+                    return
+                print("Did not receive enough help...")
+                print()
+                print("Zzzzz...")
+                time.sleep(random.randint(1, 1)) # Range changed for convenience of testing
+                print()
+                print()
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print(f"usage: {sys.argv[0]} <ID>")
-	else:
-		with Client([(SERVER_HOST, SERVER_PORT_BASE + int(port)) for port in sys.argv[1:]]) as client:
-			client.main_loop()
-		print("Byeeeee!")
+    if len(sys.argv) < 2:
+        print(f"usage: {sys.argv[0]} <ID>")
+    else:
+        with Client([(SERVER_HOST, SERVER_PORT_BASE + int(port)) for port in sys.argv[1:]]) as client:
+            client.main_loop()
+        print("Byeeeee!")
--- U1_DS/SAC_help_server.py
+++ U1_DS/SAC_help_server.py
@@ -21,82 +21,82 @@ SERVER_PORT_BASE = 9990
 
 # Override print function to show the thread it's running on
 def print(*args):
-	thread_name = threading.current_thread().name.split()[0]
-	builtins.print(("[%s] "%thread_name).ljust(20) + " ".join(map(str, args)))
+    thread_name = threading.current_thread().name.split()[0]
+    builtins.print(("[%s] "%thread_name).ljust(20) + " ".join(map(str, args)))
 
 class Helper:
-	def __init__(self, addr, client_sock):
-		self.addr = addr
-		self.client_sock = client_sock
+    def __init__(self, addr, client_sock):
+        self.addr = addr
+        self.client_sock = client_sock
 
-	def __enter__(self):
-		print("Connected " + self.addr)
-		return self
+    def __enter__(self):
+        print("Connected " + self.addr)
+        return self
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.client_sock.close()
-		print("Disconnected " + self.addr)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.client_sock.close()
+        print("Disconnected " + self.addr)
 
-	def decide_response(self):
-		value = random.randint(1, 10)
-		if value == 10:
-			print("Responding ---> [timed out: analysis paralysis]")
-			return None
-		elif value <= 4:
-			print("Responding ---> ok!")
-			return "ok!"
-		else:
-			print("Responding ---> no!")
-			return "no!"
+    def decide_response(self):
+        value = random.randint(1, 10)
+        if value == 10:
+            print("Responding ---> [timed out: analysis paralysis]")
+            return None
+        elif value <= 4:
+            print("Responding ---> ok!")
+            return "ok!"
+        else:
+            print("Responding ---> no!")
+            return "no!"
 
-	def handle_sock(self, sock):
-		data = sock.recv(1024).decode("utf-8").rstrip()
-		# Empty response means connection broke
-		if not data:
-			return True
-		print("Data received: %s"%data)
-		if data == "help!":
-			time.sleep(random.randint(1, 2))
-			response = self.decide_response()
-			if response is not None:
-				sock.sendall(response.encode("utf-8"))
-		return False
+    def handle_sock(self, sock):
+        data = sock.recv(1024).decode("utf-8").rstrip()
+        # Empty response means connection broke
+        if not data:
+            return True
+        print("Data received: %s"%data)
+        if data == "help!":
+            time.sleep(random.randint(1, 2))
+            response = self.decide_response()
+            if response is not None:
+                sock.sendall(response.encode("utf-8"))
+        return False
 
-	def selector_loop(self, sel):
-		while True:
-			events = sel.select()
-			# Ignore event masks
-			for key, _ in events:
-				callback = key.data
-				exit_loop = callback(key.fileobj)
-				if exit_loop:
-					return
+    def selector_loop(self, sel):
+        while True:
+            events = sel.select()
+            # Ignore event masks
+            for key, _ in events:
+                callback = key.data
+                exit_loop = callback(key.fileobj)
+                if exit_loop:
+                    return
 
-	def serve_requests_forever(self):
-		with selectors.DefaultSelector() as sel:
-			sel.register(self.client_sock, selectors.EVENT_READ, self.handle_sock)
-			self.selector_loop(sel)
+    def serve_requests_forever(self):
+        with selectors.DefaultSelector() as sel:
+            sel.register(self.client_sock, selectors.EVENT_READ, self.handle_sock)
+            self.selector_loop(sel)
 
 class HelperHandler(socketserver.BaseRequestHandler):
-	# When the server receives an incoming connection,
-	# this method gets executed in a dedicated thread.
-	def handle(self):
-		# We use a context manager to easily release resources on exit
-		with Helper(str(self.client_address), self.request) as helper:
-			helper.serve_requests_forever()
+    # When the server receives an incoming connection,
+    # this method gets executed in a dedicated thread.
+    def handle(self):
+        # We use a context manager to easily release resources on exit
+        with Helper(str(self.client_address), self.request) as helper:
+            helper.serve_requests_forever()
 
 # Server which spawns a new thread for each connection
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-	daemon_threads = True
-	allow_reuse_address = True
+    daemon_threads = True
+    allow_reuse_address = True
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print(f"usage: {sys.argv[0]} <ID>")
-	else:
-		port = SERVER_PORT_BASE + int(sys.argv[1])
-		print(f"Starting server on '{SERVER_HOST}:{port}'...")
-		with ThreadedTCPServer((SERVER_HOST, port), HelperHandler) as server:
-			print("Server ready!")
-			server.serve_forever()
-		print("Byeeeee!")
+    if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} <ID>")
+    else:
+        port = SERVER_PORT_BASE + int(sys.argv[1])
+        print(f"Starting server on '{SERVER_HOST}:{port}'...")
+        with ThreadedTCPServer((SERVER_HOST, port), HelperHandler) as server:
+            print("Server ready!")
+            server.serve_forever()
+        print("Byeeeee!")
